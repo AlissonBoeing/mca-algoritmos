f37173f80fab98ca2e46a5a92a44b9588396794e
223112586 <paulofylippe.sell@ge.com>
2023-09-20 21:17:26 +0000

    feat. selection sort

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ from lista import List
 
 l = List()
 
-list = [3,6,2]
-
-sl, changes, comps, duration = l.quick_sort(list) 
-
-print(sl)
-print(changes)
-print(comps)
-print(duration)
+l.measure_selection_sort()
